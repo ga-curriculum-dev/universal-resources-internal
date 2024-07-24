a03ff6cbfe64cb7ef7fca76f48f862ac9f29e57b
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2024-07-24 08:58:51 -0500

    edit: modify anchor behavior to exclude certain headers

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-  anchors.add();
+  /*
+  Add anchor links to all h2, h3, h4, and h5 elements excluding those with
+  the no-anchor class
+  */
+  anchors.add(
+    'h2:not(.no-anchor), h3:not(.no-anchor), h4:not(.no-anchor), h5:not(.no-anchor)'
+  );
   hljs.highlightAll();
 });
