dc1baff66dd63c732b5bc7aa46aed36669f12bcf
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2024-07-23 23:14:58 -0500

    edit: add sticky nav toggle

--- static/test/js/custom-defer.js
+++ static/test/js/custom-defer.js
@@ -13,6 +13,7 @@ const links = document.querySelectorAll('a')
 const pageContent = document.querySelector(".markdown-body")
 
 /* 
+Nav elements
 Check to confirm the below elements exist before using them in code to account
 for legacy modules without these elements to prevent errors.
 */ 
@@ -21,11 +22,14 @@ const subNav = document.getElementById("sub-nav-lesson-panel")
 const subNavContainer = document.querySelector(".sub-nav-container")
 const hamburgerIcon = document.querySelector(".nav-hamburger-icon")
 const closeIcon = document.querySelector(".nav-close-icon")
+const stickyNavToggle = document.getElementById("sticky-nav-toggle")
+const headerElement = document.querySelector("header.header")
 
 /* --------------------------------- State ---------------------------------- */
 
 let subNavVisible = false
 let animationInProgress = false
+let stickyNavEnabled = localStorage.getItem("stickyNav") ?? "true"
 
 /* ------------------------------- Link setup ------------------------------- */
 
@@ -69,6 +73,7 @@ if (navPanelToggle) {
   navPanelToggle.addEventListener('click', handleToggleSubNav)
   pageContent.addEventListener("click", handleInferredNavClose)
   document.body.addEventListener("keyup", handleInferredNavClose)
+  stickyNavToggle.addEventListener("click", handleToggleStickyNav)
 }
 
 function handleToggleSubNav() {
@@ -78,36 +83,39 @@ function handleToggleSubNav() {
   } else {
     showNav()
   }
-  
 }
 
 function showNav() {
   animationInProgress = true
+
   subNav.classList.add("visible")
   subNavContainer.classList.add("open")
   navPanelToggle.setAttribute("aria-expanded", "true")
   navPanelToggle.setAttribute("aria-label", "Close navigation")
   hamburgerIcon.classList.remove("visible")
   closeIcon.classList.add("visible")
+
   setTimeout(function() {
+    subNavVisible = true
     animationInProgress = false
   }, 351)
-  subNavVisible = true
 }
 
 function hideNav() {
   animationInProgress = true
+
   navPanelToggle.setAttribute("aria-expanded", "false")
   navPanelToggle.setAttribute("aria-label", "Open navigation")
   closeIcon.classList.remove("visible")
   hamburgerIcon.classList.add("visible")
   subNavContainer.classList.remove("open")
+
   // wait until close animation is complete before hiding element
   setTimeout(function() {
     subNav.classList.remove("visible")
+    subNavVisible = false
     animationInProgress = false
   }, 351)
-  subNavVisible = false
 }
 
 function handleInferredNavClose(evt) {
@@ -115,4 +123,37 @@ function handleInferredNavClose(evt) {
   if (evt.type === "click" || (evt.type === "keyup" && evt.key === "Escape")) {
     handleToggleSubNav()
   } 
+}
+
+/* ------------------------ Sticky nav functionality ------------------------ */
+
+/*
+Not all users may want to use the sticky nav functionality so we allow them to
+toggle the functionality off and on, and persist that choice in localStorage.
+*/
+
+function handleToggleStickyNav() {
+  if (stickyNavEnabled === "true") {
+    localStorage.setItem("stickyNav", "false")
+    stickyNavEnabled = "false"
+  } else {
+    localStorage.setItem("stickyNav", "true")
+    stickyNavEnabled = "true"
+  }
+  renderStickyNavButton()
+}
+
+function renderStickyNavButton() {
+  /* 
+  Check to confirm the navPanelToggle element exists to account for legacy 
+  modules without nav functionality to prevent errors.
+  */ 
+  if (!navPanelToggle) return
+  if (stickyNavEnabled === "true") {
+    stickyNavToggle.textContent = "Turn off sticky nav"
+    headerElement.classList.remove("no-stick")
+  } else if (stickyNavEnabled === "false") {
+    stickyNavToggle.textContent = "Turn on sticky nav"
+    headerElement.classList.add("no-stick")
+  }
 }
