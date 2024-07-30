c58e71a4b8c44eece739c8b23087b79671cbae44
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2024-07-29 22:14:50 -0500

    draft: add custom

---
+++ static/test/js/custom.js
@@ -0,0 +1,12 @@
+import "./page-build.js"
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  /*
+  Add anchor links to all h2, h3, h4, and h5 elements excluding those with
+  the no-anchor class
+  */
+  anchors.add(
+    'h2:not(.no-anchor), h3:not(.no-anchor), h4:not(.no-anchor), h5:not(.no-anchor)'
+  );
+  hljs.highlightAll();
+});
