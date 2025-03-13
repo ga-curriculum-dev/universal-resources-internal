f82d1112bc5d6ab69ee6d772fda9ad8e49ac3d3a
David Stinson <mail@davidstinson.dev>
2025-03-13 15:26:12 -0500

    fix: codepen cl display (#16)

--- static/js/pre-work-codepen-script.js
+++ static/js/pre-work-codepen-script.js
@@ -1,8 +1,8 @@
 const console = {
-  log: (msg) => {
+  log: (...msg) => {
     const output = document.createElement('output')
     const pre = document.createElement('pre')
-    output.textContent = `Console log output: ${msg}`
+    output.textContent = `Console log output: ${msg.join(', ')}`
     pre.appendChild(output)
     document.body.appendChild(pre)
   }
